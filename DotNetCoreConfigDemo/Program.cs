f23bdf91a86391ae4051de81e25814dab77086f5
李凤焕 <huanent@hotmail.com>
2017-08-30 16:41:33 +0800

    切换到2.0

--- DotNetCoreConfigDemo/Program.cs
+++ DotNetCoreConfigDemo/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DotNetCoreConfigDemo
 {
@@ -8,11 +11,21 @@ namespace DotNetCoreConfigDemo
     {
         static void Main(string[] args)
         {
-            var myConfig = new MyConfig();
-            new ConfigurationBuilder()
-                .AddJsonFile("appsetting.json")
-                .Build()
-                .Bind(myConfig);
+            var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsetting.json", false, true)
+                 .Build();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<MyConfig>(config);
+            var services = serviceCollection.BuildServiceProvider();
+
+            while (true)
+            {
+                var myConfig = services.GetService<IOptionsMonitor<MyConfig>>();
+                Task.Delay(1000).Wait();
+                Console.WriteLine(myConfig.CurrentValue.Age);
+            }
         }
     }
 }
